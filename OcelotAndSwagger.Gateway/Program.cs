b2ad7c405d1b515b81aebe5bf4ffcf2b28af3bd0
Mostafa Rowghanian <mostafatech@gmail.com>
2019-07-02 11:05:05 +0430

    Add project files.

---
+++ OcelotAndSwagger.Gateway/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace OcelotAndSwagger.Gateway
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      CreateWebHostBuilder(args).Build().Run();
+    }
+
+    public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+        WebHost.CreateDefaultBuilder(args)
+            .UseContentRoot(Directory.GetCurrentDirectory())
+            .ConfigureAppConfiguration((hostingContext, config) =>
+            {
+              config
+                  .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
+                  .AddJsonFile("appsettings.json", true, true)
+                  .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true)
+                  .AddJsonFile("ocelot.json")
+                  .AddEnvironmentVariables();
+            })
+            .UseIIS()
+            .UseStartup<Startup>();
+  }
+}
---
+++ OcelotAndSwagger.Gateway/Startup.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+namespace OcelotAndSwagger.Gateway
+{
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
+    {
+      Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+      services.AddSwaggerGen();
+
+      services.AddOcelot();
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+
+
+      app.UseMvcWithDefaultRoute();
+
+      app.UseSwagger();
+      app.UseSwaggerUI(c =>
+      {
+        c.SwaggerEndpoint("/service1/swagger/v1/swagger.json", "Service 1");
+        c.SwaggerEndpoint("/service2/swagger/v1/swagger.json", "Service 2");
+      });
+
+      app.UseOcelot().Wait();
+    }
+  }
+}
